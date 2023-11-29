2faa66f2c925aaf16a43bcb7e8e331c5892bb0ec
Rahul Vaniya <vaniyarahul277@gmail.com>
2023-11-29 12:40:04 +0530

    Update main.dart maid changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
-//b e1
+//b e v3
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-import 'dart:async';
-//import 'dart:convert';
-const api_key = "AIzaSyAQstU4fdpXbjrzNkgayr_seWo_cWR9fbg";
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -24,7 +21,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final TextEditingController _answerController = TextEditingController();
-  String apiUrl = 'https://generativelanguage.googleapis.com/v1beta2/models/text-bison-001:generateText?key= $api_key';
+  String apiUrl = 'https://generativelanguage.googleapis.com/v1beta2/models/text-bison-001:generateText?key=AIzaSyAQstU4fdpXbjrzNkgayr_seWo_cWR9fbg';
   String responseText = '';
   String directSpeechSentence = '';
   bool isLoading = true;
@@ -40,11 +37,12 @@ class _MyHomePageState extends State<MyHomePage> {
       isLoading = true;
     });
 
-String getPromt='Student is learning how to convert direct speech to indirect speech. For practice purposes please give only one sentence which is direct speech and which can be converted into indirect speech.';
+    String getPrompt =
+        'Student is learning how to convert direct speech to indirect speech. For practice purposes, please give only one sentence which is direct speech and which can be converted into indirect speech. Please, only give back the sentence and no other text.';
     final response = await http.post(
       Uri.parse(apiUrl),
       headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({'prompt': getPromt}),
+      body: jsonEncode({'prompt': {'text': getPrompt}}),
     );
 
     if (response.statusCode == 200) {
@@ -67,25 +65,32 @@ String getPromt='Student is learning how to convert direct speech to indirect sp
       isLoading = true;
     });
 
+    String prompt =
+        'Student is learning how to convert direct speech to indirect speech. following is the original sentence in direct speech: $directSpeechSentence Is the following indirect speech correct? ${_answerController.text} Your response should be in JSON format. that JSON must contain a property named isCorrect. It should be true if the answer is true. else it should be false. If the answer is incorrect, please include explanation in text property of that JSON. Please do not respond in any other way then JSON.';
+
     final response = await http.post(
       Uri.parse(apiUrl),
       headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({'prompt': 'Student is learning how to convert direct speech to indirect speech. following is the original sentence in direct speech: $directSpeechSentence Is the following indirect speech correct? $_answerController.text'}),
+      body: jsonEncode({'prompt': {'text': prompt}}),
     );
 
     if (response.statusCode == 200) {
       Map<String, dynamic> data = jsonDecode(response.body);
-      List<dynamic> candidates = data['candidates'];
-      String generatedText = candidates.isNotEmpty ? candidates[0]['output'] : '';
-
-      // Compare the generated text with the user's answer
-      String userAnswer = _answerController.text;
-      bool isCorrect = generatedText.contains(userAnswer);
+      bool isCorrect = data['isCorrect'] == true;
+      String generatedText = data['text'];
 
       setState(() {
         isLoading = false;
         responseText = isCorrect ? 'Correct! $generatedText' : 'Incorrect. $generatedText';
       });
+
+      if (isCorrect) {
+        // Show button only if the answer is incorrect
+        // Fetch another sentence if the answer is correct
+var future = new Future.delayed(const Duration(milliseconds: 1500),         getDirectSpeech);
+
+//        getDirectSpeech();
+      }
     } else {
       setState(() {
         isLoading = false;
@@ -98,7 +103,7 @@ String getPromt='Student is learning how to convert direct speech to indirect sp
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Direct-Indirect Speech Practice'),
+        title: liveText('Direct-Indirect Speech Practice'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -106,7 +111,7 @@ String getPromt='Student is learning how to convert direct speech to indirect sp
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             if (isLoading) CircularProgressIndicator(),
-            if (!isLoading) Text('Direct Speech: $directSpeechSentence'),
+            if (!isLoading) liveText('Direct Speech: $directSpeechSentence'),
             SizedBox(height: 20),
             TextField(
               controller: _answerController,
@@ -117,11 +122,24 @@ String getPromt='Student is learning how to convert direct speech to indirect sp
               onPressed: checkAnswer,
               child: Text('Check Answer'),
             ),
+            if (!isLoading && !isCorrect)
+              ElevatedButton(
+                onPressed: getDirectSpeech,
+                child: Text('Get Next Sentence'),
+              ),
             SizedBox(height: 20),
-            Text(responseText),
+            liveText(responseText),
           ],
         ),
       ),
     );
+  }
+}
+class LiveText extends StatelessWidget{
+  final String text;
+  LiveText(this.text);
+  @override
+  Widget build(BuildContext context) {
+return Semantics(child:Text(text), liveRegion: true,);
   }
 }
