8a363bc302125474eef1bad8181c77037cf4576c
Rahul Vaniya <vaniyarahul277@gmail.com>
2023-11-29 19:08:15 +0530

    Update main.dart t

--- lib/main.dart
+++ lib/main.dart
@@ -39,11 +39,11 @@ class _MyHomePageState extends State<MyHomePage> {
     });
 
     String getPrompt =
-        'Student is learning how to convert direct speech to indirect speech. For practice purposes, please give only one sentence which is direct speech and which can be converted into indirect speech. Please, only give back the sentence and no other text.';
+        'Student is learning how to convert direct speech to reported speech. For practice purposes, please give only one sentence which is direct speech and which can be converted into reported speech. Please, only give back the sentence and no other text.';
     final response = await http.post(
       Uri.parse(apiUrl),
       headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({'prompt': {'text': getPrompt}}),
+      body: jsonEncode({'temperature': '0.25', 'prompt': {'text': getPrompt}}),
     );
 
     if (response.statusCode == 200) {
@@ -66,22 +66,25 @@ class _MyHomePageState extends State<MyHomePage> {
       isLoading = true;
     });
 
-    String prompt =
-        'Student is learning how to convert direct speech to indirect speech. following is the original sentence in direct speech: $directSpeechSentence Is the following indirect speech correct? ${_answerController.text} Your response should be in JSON format. that JSON must contain a property named isCorrect. It should be true if the answer is true. else it should be false. If the answer is incorrect, please include explanation in text property of that JSON. Please do not respond in any other way then JSON.';
+    String prompt = 'Student is learning how to convert direct speech to reported speech. Following is the original sentence in direct speech: $directSpeechSentence Is the following conversion to reported speech from direct speech correct? ${_answerController.text} Your response should be in JSON format. That JSON must contain a property named isCorrect. It should be true if the answer is true. Else it should be false. If the answer is incorrect, please include explanation in text property of that JSON. Please do not respond in any other way then JSON.';
 
     final response = await http.post(
       Uri.parse(apiUrl),
       headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({'prompt': {'text': prompt}}),
+      body: jsonEncode({'temperature': '0.25', 'prompt': {'text': prompt}}),
     );
 
     if (response.statusCode == 200) {
       Map<String, dynamic> resData = jsonDecode(response.body);
+print(resData);
       List<dynamic> candidates = resData['candidates'];
+print(candidates);
        var sdata = candidates.isNotEmpty ? candidates[0]['output'] : '';
+print(sdata);
       Map<String, dynamic> data = jsonDecode(sdata);
-      isCorrect = data['isCorrect'] == true;
-      String generatedText = data['text'];
+print(data);
+      isCorrect = data['isCorrect'];
+      String generatedText = data['text']?data['text']:'';
 
       setState(() {
         isLoading = false;
@@ -91,7 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
       if (isCorrect) {
         // Show button only if the answer is incorrect
         // Fetch another sentence if the answer is correct
-new Future.delayed(const Duration(milliseconds: 1500),         getDirectSpeech);
+Future.delayed(const Duration(milliseconds: 1500),         getDirectSpeech);
 
 //        getDirectSpeech();
       }
